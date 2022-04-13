e77c5e53e50c4371082057c78f1d29e94fd2d73e
mavargas <marco12084@gmail.com>
2022-04-12 20:41:33 -0500

    Hola Mundo

--- BashParaExamen.sh
+++ BashParaExamen.sh
@@ -5,6 +5,11 @@ BuscarUsuario(){
 	set Auxiliar =$(groups $Usuario)
 	if $Auxiliar &>/dev/null/; then
 		echo "El usuario no existe en los grupos"
+		echo "Desea agregar el usuario a algun grupo?"
+		echo "1)Si"
+		echo "2)No"
+		set Opcion
+		read Opcion
 	else
 		echo "Grupos del Usuario: ";echo "$Auxiliar"
 	fi
