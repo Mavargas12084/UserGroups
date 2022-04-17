3b1333fea99f936eaa5c5fab660fe54cd61e7e5c
mavargas <marco12084@gmail.com>
2022-04-17 15:05:13 -0500

    Ultimos cambios

--- BashParaExamen.sh
+++ BashParaExamen.sh
@@ -10,6 +10,7 @@ ValidarUsuario(){
 		echo "Deseas Agregarlo?..."
 		echo "1)Si"
 		echo "2)No"
+		OpcionValidar=0
 		read OpcionValidar
 		if [ $OpcionValidar -eq "1" ]; then
 			sudo adduser $1 
@@ -32,6 +33,7 @@ BuscarUsuario(){
 		echo "Deseas agregarlo a algun grupo?..."
 		echo "1)Si"
 		echo "2)No"
+		OpcionBusqueda=0
 		read OpcionBusqueda
 		if [ $OpcionBusqueda -eq "1" ]; then
 			clear
@@ -51,6 +53,7 @@ EliminarUsuario(){
 	echo "Desea Eliminar Usuario $1 ??"
 	echo "1)Si"
 	echo "2)No"
+	OpcionEliminar=0
 	read OpcionEliminar
 	if [ $OpcionEliminar -eq "1" ]; then
 		sudo userdel -f $1
@@ -60,12 +63,30 @@ EliminarUsuario(){
 	fi
 }
 
+CambiarPassword(){
+	echo "Se va a cambiar el password del Usuario $1"
+	echo "1)Aceptar"
+	echo "2)Cancelar"
+	echo "Seleccione una opcion:"
+	OpcionPass=0
+	read OpcionPass
+	if [ $OpcionPass -eq "1" ]; then
+		passwd $1
+		echo "Se Cambio el password de $1"
+	elif [ $OpcionPass -eq "2" ]; then
+		echo "Se Cancela Cambio de Password"
+	else
+		echo "Opcion no valida!"
+	fi
+}
+
 echo "Bienvenido $1"
 echo "=========== Manejo de Usuarios ============="
 echo "Seleccione una Opcion Valida:"
 echo "1)Validar Existencia de Usuario"
 echo "2)Validar Existencia de Usuario en Grupos"
 echo "3)Eliminar Usuario"
+echo "4)Cambio de Password "
 echo "Ingrese una Opcion: "
 OpcionMenu=0
 read OpcionMenu
@@ -82,5 +103,9 @@ case $OpcionMenu in
 	3)
 		clear
 		EliminarUsuario
+	;;
+	4)
+		clear
+		CambiarPassword()
 	;;
 esac
